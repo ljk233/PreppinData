5c67fec8eeb403ae6b7089a9707298dc15e36d0c
Liam Kelly <liam.james.kelly@gmail.com>
2024-03-23 08:40:20 +0000

    Updated docstring

--- preppin_data/common_expressions.py
+++ preppin_data/common_expressions.py
@@ -4,9 +4,32 @@
 import polars as pl
 
 
-def parse_str_to_bool(col_name: str, true_label: str) -> pl.Expr:
+def parse_str_to_bool(col_name: str, true_label: str, col_alias: str) -> pl.Expr:
+    """Convert string values in a column to boolean values based on a specified true label.
+
+    Parameters
+    ----------
+    col_name : str
+        The name of the column containing string values to be converted.
+    true_label : str
+        The label in the column indicating true values.
+    col_alias : str
+        The alias to assign to the resulting boolean column.
+
+    Returns
+    -------
+    pl.Expr
+        A Polars expression representing the transformation.
+
+    Notes
+    -----
+    This function creates a Polars expression that evaluates to True for
+    rows where the value in the specified column matches the true label,
+    and False otherwise. The resulting boolean olumn is given the specified alias.
+    """
     return (
         pl.when(pl.col(col_name) == true_label)
         .then(pl.lit(True))
         .otherwise(pl.lit(False))
+        .alias(col_alias)
     )
